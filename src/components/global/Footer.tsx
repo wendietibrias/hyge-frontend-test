46509b070ea7251adb0acff28164d4d494d16a65
wendietibrias <wendietibrias@gmail.com>
2024-04-09 17:41:39 +0700

    fix: footer layout in production

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="w-full   border-top border-white/10 py-10  z-[999] lg:px-10 sm:px-5">
             <div className="flex items-start gap-x-14 sm:w-full max-w-[1300px] mx-auto sm:flex-col lg:gap-y-10 flex-wrap">
-                <div className="w-[330px] lg:w-[40%]">
+                <div className="w-[330px] sm:w-full lg:w-[40%]">
                   <Image src="/static/assets/images/company_logo.png" alt="Hyge" width={100} height={28}/>
                   <p className="text-[15px] my-6 text-description">
                    Hyge is a Singapore based company that provides customisation and specialisation in website design, development, as well as mobile app development. 
@@ -15,7 +15,7 @@ const Footer = () => {
                   </h5>
                 </div>
 
-                <div className=" text-description lg:w-[40%] flex flex-col gap-y-5">
+                <div className=" text-description sm:w-full lg:w-[40%] flex flex-col gap-y-5">
                     <div>
                         <p className="text-sm">Email us : </p>
                         <h5 className="font-semibold mt-2">michael@hygeworks.com</h5>
@@ -26,8 +26,8 @@ const Footer = () => {
                     </div>
                 </div>
 
-                <div className="flex-1 lg:w-full flex items-start lg:justify-start justify-end">
-                   <div className="w-[250px]  sm:w-full flex flex-col gap-y-5 text-description">
+                <div className="flex-1 lg:w-full  flex items-start lg:justify-start justify-end">
+                   <div className="w-[250px] sm:w-full flex flex-col gap-y-5 text-description">
                        <button className="w-full border-b pb-3 border-description flex items-center justify-between">
                          Linkedin
                          <Image src="/static/assets/icons/arrow.png" alt="arrow icon" width={24} height={24}/>
